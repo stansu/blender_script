c0b1b1dc218df409a5cc18749ec51d11e324f113
stansu <stan.stz@gmail.com>
2016-12-05 16:11:25 +0800

    Create Create_Internal_Mats.py

---
+++ Create_Internal_Mats.py
@@ -0,0 +1,61 @@
+# import modules
+import bpy
+
+# create a object contain all materials
+mats = bpy.data.materials
+
+# make sure all materials use nodes
+for i in mats:
+    if not i.use_nodes:
+        i.use_nodes = True
+
+# start looping all materials
+for i in mats:
+    
+    # check material have (Image Texture) node
+    if i.node_tree.nodes.get('Image Texture'):
+    
+        # create internal material
+        newOutput = i.node_tree.nodes.new('ShaderNodeOutput')
+        newMat = i.node_tree.nodes.new('ShaderNodeMaterial')
+        
+        # create new links
+        newOutputIn = newOutput.input[0]
+        newMatOut = newMat.outputs[0]
+        newMatIn = newMat.inputs[0]
+        ImgOut = i.node_tree.nodes.get('Image Texture').outputs[0]
+        i.node_tree.links.new(ImgOut, newMatIn)
+        
+        # create original linkin objects
+        #old_BSDF_out = i.node_tree.nodes['Material Output'].inputs[0].links[0].from_socket
+        #surf_in = i.node_tree.nodes['Material Output'].inputs[0]
+        
+        # check material have (stan_mix) node
+        #if not i.node_tree.nodes.get('stan_mix'):
+            
+            # if not, create stan_* nodes
+            stan_mix = i.node_tree.nodes.new('ShaderNodeMixShader')
+            stan_mix.name = 'stan_mix'
+            stan_mix.inputs[0].default_value = 0
+            stan_mix_in_s1 = stan_mix.inputs[1]
+            stan_mix_in_s2 = stan_mix.inputs[2]
+            stan_mix_out_s = stan_mix.outputs[0]
+            stan_emit = i.node_tree.nodes.new('ShaderNodeEmission')
+            stan_emit.name = 'stan_emit'
+            temp_color = [random.random() for i in range(4)]
+            temp_color[3] = 1
+            stan_emit.inputs[0].default_value = temp_color
+            stan_emit_out = stan_emit.outputs[0]
+            
+            # create new links
+            i.node_tree.links.new(stan_emit_out, stan_mix_in_s2)
+            i.node_tree.links.new(old_BSDF_out, stan_mix_in_s1)
+            i.node_tree.links.new(stan_mix_out_s, surf_in)
+        else:
+            stan_mix = i.node_tree.nodes['stan_mix']
+        
+        # toggle emit materials
+        if stan_mix.inputs[0].default_value == 0:
+            stan_mix.inputs[0].default_value = 1
+        else:
+            stan_mix.inputs[0].default_value = 0
