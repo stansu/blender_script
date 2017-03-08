f2da531ad92c44e1060af9ada83e51e817d43195
stansu <stan.stz@gmail.com>
2017-03-08 16:45:24 +0800

    simplfy ui

--- espeak.py
+++ espeak.py
@@ -9,34 +9,26 @@ bl_info = {
 import bpy
 import subprocess
 
-class TextToVoice(bpy.types.Operator):
-    bl_idname = "object.speak"
-    bl_label = "Speak"
-    
-    def execute(self, context):
-        path='/usr/bin/espeak'
-        subprocess.run([path, '-s120', bpy.context.scene.speak_string])
-        return{'FINISHED'}
-
-def addon_button(self, context):
-    self.layout.operator(
-          "object.speak",
-          text="Speak",)
+def add_string(self, context):
     self.layout.prop(context.scene, "speak_string")
 
+def speakText(self, context):
+    path='/usr/bin/espeak'
+    subprocess.run([path, '-s120', bpy.context.scene.speak_string])
+    return{'FINISHED'}
+
 def register():
-    bpy.utils.register_class(TextToVoice)
     bpy.types.Scene.speak_string = bpy.props.StringProperty \
       (
         name = "",
         description = "type text here to speak",
-        default = "Type text to speak"
+        default = "Type text to speak",
+        update = speakText
       )
-    bpy.types.INFO_HT_header.append(addon_button)
+    bpy.types.INFO_HT_header.append(add_string)
 
 def unregister():
-    bpy.utils.unregister_class(TextToVoice)
-    bpy.types.INFO_HT_header.remove(addon_button)
+    bpy.types.INFO_HT_header.remove(add_string)
     del bpy.types.Scene.speak_string
 
 if __name__ == "__main__":
