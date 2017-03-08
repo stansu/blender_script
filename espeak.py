f6b3bf84bc817ccab308d968029af90fecce6a58
stansu <stan.stz@gmail.com>
2017-03-08 16:23:03 +0800

    first try success

--- espeak.py
+++ espeak.py
@@ -1,3 +1,11 @@
+bl_info = {
+    "name": "Speak English",
+    "author": "stan",
+    "version": (1, 0),
+    "blender": (2, 78, 0),
+    "description": "Speak English",
+    "category": "User Interface"}
+
 import bpy
 import subprocess
 
@@ -5,28 +13,10 @@ class TextToVoice(bpy.types.Operator):
     bl_idname = "object.speak"
     bl_label = "Speak"
     
-#    text = bpy.props.StringProperty()
-#    text = "Type text to speak"
- 
     def execute(self, context):
-#        path='C:\Program Files (x86)\eSpeak\command_line\espeak.exe'
-#        subprocess.run([path, '-s120', self.text])
+        path='/usr/bin/espeak'
+        subprocess.run([path, '-s120', bpy.context.scene.speak_string])
         return{'FINISHED'}
-
-class SpeakMakePanel(bpy.types.Panel):
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
-#    bl_context = "objectmode"
-    bl_category = "stan"
-    bl_label = "Speak"
-        
-    def draw(self, context):
-#        TheCol = self.layout.column(align=True)
-        TheCol = self.layout
-        TheCol.prop(context.scene, "speak_string")
-        TheCol.operator("object.speak", text="Speak")
-#        TheCol.props(self.text)
-
 
 def addon_button(self, context):
     self.layout.operator(
@@ -36,10 +26,9 @@ def addon_button(self, context):
 
 def register():
     bpy.utils.register_class(TextToVoice)
-#    bpy.utils.register_class(SpeakMakePanel)
     bpy.types.Scene.speak_string = bpy.props.StringProperty \
       (
-        name = "text_to_speak",
+        name = "",
         description = "type text here to speak",
         default = "Type text to speak"
       )
@@ -47,7 +36,6 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(TextToVoice)
-#    bpy.utils.unregister_class(SpeakMakePanel)
     bpy.types.INFO_HT_header.remove(addon_button)
     del bpy.types.Scene.speak_string
 
