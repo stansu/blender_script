aed04c6d9fe0e7fa241cc9b90fdb0cae695e51f9
stansu <stan.stz@gmail.com>
2017-03-23 18:07:14 -0500

    Add other state check

--- manipulatorCycle.py
+++ manipulatorCycle.py
@@ -5,7 +5,7 @@ for area in bpy.context.screen.areas:
         for space in area.spaces:
             if space.type == 'VIEW_3D':
                 space.show_manipulator = True
-                if space.transform_manipulators == {'TRANSLATE'}:
+                if space.transform_manipulators != {'TRANSLATE'}:
                     space.transform_manipulators = {'ROTATE'}
                 elif space.transform_manipulators == {'ROTATE'}:
                     space.transform_manipulators = {'SCALE'}
