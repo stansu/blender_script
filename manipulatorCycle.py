7e6d20ef48a4b04b05ceaa1fa87455a6ae404c01
stansu <stan.stz@gmail.com>
2017-03-24 09:44:00 +0800

    correct all condition's check.

--- manipulatorCycle.py
+++ manipulatorCycle.py
@@ -5,11 +5,11 @@ for area in bpy.context.screen.areas:
         for space in area.spaces:
             if space.type == 'VIEW_3D':
                 space.show_manipulator = True
-                if space.transform_manipulators != {'TRANSLATE'}:
-                    space.transform_manipulators = {'TRANSLATE'}
-                elif space.transform_manipulators == {'TRANSLATE'}:
+                if space.transform_manipulators == {'TRANSLATE'}:
                     space.transform_manipulators = {'ROTATE'}
                 elif space.transform_manipulators == {'ROTATE'}:
                     space.transform_manipulators = {'SCALE'}
                 elif space.transform_manipulators == {'SCALE'}:
+                    space.transform_manipulators = {'TRANSLATE'}
+                else:
                     space.transform_manipulators = {'TRANSLATE'}
