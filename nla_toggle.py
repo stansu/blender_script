da8c6f4b54bcd5a6701935a2056c36922fc8a63a
stansu <stan.stz@gmail.com>
2018-02-27 23:34:28 +0800

    Change to selected objects

--- nla_toggle.py
+++ nla_toggle.py
@@ -1,6 +1,6 @@
 import bpy
 
-for i in bpy.data.objects:
+for i in bpy.context.selected_objects:
     for j in i.animation_data.nla_tracks.values():
         for k in j.strips.values():
             if k.select:
