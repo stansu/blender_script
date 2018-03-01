ce45f6fe185967c26d31063490a4aea5306eae5b
stansu <stan.stz@gmail.com>
2018-03-01 15:13:50 +0800

    simplfy nla_toggle.py

--- nla_toggle.py
+++ nla_toggle.py
@@ -2,7 +2,8 @@ import bpy
 
 for i in bpy.data.objects:
     if i.animation_data is not None:
-        for j in i.animation_data.nla_tracks.values():
-            for k in j.strips.values():
+        for j in i.animation_data.nla_tracks:
+            for k in j.strips:
                 if k.select:
                     k.use_reverse = not k.use_reverse
+
