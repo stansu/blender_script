2e3601b88485475dfbc8dd5423d89e49d14af0b6
stansu <stan.stz@gmail.com>
2018-03-01 09:37:42 +0800

    add check nla avalible

--- nla_toggle.py
+++ nla_toggle.py
@@ -1,7 +1,8 @@
 import bpy
 
-for i in bpy.context.selected_objects:
-    for j in i.animation_data.nla_tracks.values():
-        for k in j.strips.values():
-            if k.select:
-                k.use_reverse = True
+for i in bpy.data.objects:
+    if type(i.animation_data) != type(None) :
+        for j in i.animation_data.nla_tracks.values():
+            for k in j.strips.values():
+                if k.select:
+                    k.use_reverse = not k.use_reverse
