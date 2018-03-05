719f737772b5e2c63f42d949aaa04d0cd1ad8958
stansu <stan.stz@gmail.com>
2018-03-05 11:28:29 +0800

    add updateTissue.py

---
+++ updateTissue.py
@@ -0,0 +1,17 @@
+import bpy
+
+tempActive = bpy.context.active_object
+tempFirst = True
+
+for o in bpy.data.objects:
+    if o.tissue_tessellate.generator != '':
+        if tempFirst:
+            bpy.context.scene.objects.active = o
+            bpy.ops.object.update_tessellate()
+            bpy.ops.object.update_tessellate()
+            tempFirst = False
+        else:
+            bpy.context.scene.objects.active = o
+            bpy.ops.object.update_tessellate()
+
+bpy.context.active_object = tempActive
