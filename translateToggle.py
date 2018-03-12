4b47c3721ec3336f971f1b31506544fa551abc00
stansu <stan.stz@gmail.com>
2018-03-12 11:30:56 +0800

    correct toogles

--- translateToggle.py
+++ translateToggle.py
@@ -11,26 +11,25 @@ bl_info = {
 
 import bpy
 
+bpy.context.user_preferences.system.use_international_fonts = True
+
 class Translation(bpy.types.Operator):
     """Toggle UI translation"""
     bl_idname = "screen.translation"
     bl_label = "Interface translation"
 
+    toggleState = True
+    
     def execute(self, context):
-        bpy.context.user_preferences.system.use_international_fonts = True
-        interface_translate = bpy.context.user_preferences.system.use_translate_interface
-        tooltips_translate = bpy.context.user_preferences.system.use_translate_tooltips
-        new_data_translate = bpy.context.user_preferences.system.use_translate_new_dataname
-        bpy.context.user_preferences.system.use_translate_interface = not interface_translate
-        bpy.context.user_preferences.system.use_translate_tooltips = not tooltips_translate
-        bpy.context.user_preferences.system.use_translate_new_dataname = not new_data_translate
+        bpy.context.user_preferences.system.use_translate_interface = not bpy.context.user_preferences.system.use_translate_interface
+        bpy.context.user_preferences.system.use_translate_tooltips = not bpy.context.user_preferences.system.use_translate_tooltips
+        bpy.context.user_preferences.system.use_translate_new_dataname = not bpy.context.user_preferences.system.use_translate_new_dataname
         return {'FINISHED'}
 
 def addon_button(self, context):
      self.layout.operator(
           "screen.translation",
           text="Translation",)
-
 
 def register():
     bpy.utils.register_class(Translation)
