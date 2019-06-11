4a34666b10e63ad7a952b3d9d55c965b7ad575d7
stansu <stan.stz@gmail.com>
2019-06-11 14:05:26 +0800

    Update import_multi_fbx.py

--- import_multi_fbx.py
+++ import_multi_fbx.py
@@ -3,9 +3,7 @@ import glob
 import os
 
 imd = '/home/stan/Downloads'
-print(imd)
 os.chdir(imd)
 
 for files in glob.glob("*.fbx"):
-    print( files )
     bpy.ops.import_scene.fbx(filepath=files)
