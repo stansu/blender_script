ad5cf8ba0de04cc8ced4d5cbabe138641b5dfa99
stansu <stan.stz@gmail.com>
2021-08-31 13:39:00 +0800

    add exist check
    add exist check

--- check_bones_name.py
+++ check_bones_name.py
@@ -1,10 +1,16 @@
 import bpy, os
 
 tempString = ''
-tempFile = os.path.splitext(bpy.path.basename(bpy.data.filepath))[0]
-bpy.data.texts.new(tempFile)
+
 for a in bpy.data.armatures:
     for b in a.bones:
         tempString += a.name + ',' + b.name + '\n'
 
-bpy.data.texts[tempFile].from_string(tempString)
+tempFile = os.path.splitext(bpy.path.basename(bpy.data.filepath))[0]
+
+if bpy.data.texts.find(tempFile) == -1:
+    bpy.data.texts.new(tempFile)
+    bpy.data.texts[tempFile].from_string(tempString)
+else:
+    bpy.data.texts[tempFile].from_string(tempString)
+    
